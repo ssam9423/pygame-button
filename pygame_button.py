a22964107c83620b5106811ffdd30a2cac775b3f
ssam9423 <80365693+ssam9423@users.noreply.github.com>
2025-01-21 12:47:34 -0800

    Update pygame_button.py

--- pygame_button.py
+++ pygame_button.py
@@ -1,32 +1,36 @@
 # Button Module for Pygame
-
 import pygame
 
 class Button:
-    def __init__(self, name, screen, bg_color, t_color, x_pos, y_pos):
+    def __init__(self, name, screen, bg_color, t_color, x_pos, y_pos,
+                 hover_bg_color=(96, 108, 56), hover_t_color=(96, 108, 56), 
+                 default=0, hover=2,
+                 font_name='yugothicuisemibold', font_size=25,
+                 width=150, height=80, spacing_factor=10,
+                 b_radius=2, static_size=False):
         self.name = name
         self.screen = screen
         self.bg_color = bg_color
         self.t_color = t_color
-        self.hover_bg_color = bg_color
-        self.hover_t_color = bg_color
-        self.default = 0                # Button Color Fill
-        self.hover = 2                  # Button Color Fill
+        self.hover_bg_color = hover_bg_color
+        self.hover_t_color = hover_t_color
+        self.default = default          # Button Color Fill
+        self.hover = hover              # Button Color Fill
 
-        self.font_name = 'yugothicuisemibold'
-        self.font_size = 20
+        self.font_name = font_name
+        self.font_size = font_size
 
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.w_factor = 5
-        self.h_factor = 10
-        self.spacing_factor = 15
-        self.width = self.screen.get_size()[0] / self.w_factor
-        self.height = self.screen.get_size()[1] / self.h_factor
+        self.width = width
+        self.height = height
+        self.w_factor = self.screen.get_size()[0] / self.width
+        self.h_factor = self.screen.get_size()[1] / self.height
+        self.spacing_factor = spacing_factor
         self.w_spacing = self.width / self.spacing_factor
         self.h_spacing = self.height / self.spacing_factor
-        self.b_radius = 2
-        self.static_size = False
+        self.b_radius = b_radius
+        self.static_size = static_size
         
         self.rect = pygame.Rect(self.x_pos, self.y_pos, self.width, self.height)
         self.font = pygame.font.SysFont(self.font_name, self.font_size)
@@ -66,8 +70,8 @@ class Button:
         curr_size = int(self.height)
         while curr_size > 1:
             f_w, f_h = pygame.font.SysFont(self.font_name, curr_size).size(self.name)
-            if ((f_w < (self.width - (2 * self.spacing_factor))) and
-                (f_h < (self.height - (2 * self.spacing_factor)))):
+            if ((f_w < (self.width - (2 * self.w_spacing))) and
+                (f_h < (self.height - (2 * self.h_spacing)))):
                 break
             else:
                 curr_size -= 1
@@ -80,7 +84,7 @@ class Button:
         self.h_spacing = self.height / new_spacing
 
     def get_size_offset(self):
-        return (self.width/2, self.height/2)
+        return (int(self.width/2), int(self.height/2))
     
     def get_size(self):
         return (self.width, self.height)
